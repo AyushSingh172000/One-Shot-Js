f26f2293b6f135daba2ac807e4616d2da6fae585
Ayush Kumar Singh <139788147+AyushSingh172000@users.noreply.github.com>
2026-01-12 12:20:01 +0000

    basic variables

---
+++ Variables.js
@@ -0,0 +1,7 @@
+const accountId = 121
+let accountEmail = "ayush@gmail.com"
+var accountPass = "123"
+accounCity = "Varanasi"
+//accountId= 2
+console.log(accountId)
+console.table([accountEmail,accountId,accountPass,accounCity])
